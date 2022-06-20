bef65c283d7ef9edade2ceda5a6d1b4b686d4f1d
Mikael <dev.kov.mtl@gmail.com>
2022-06-20 16:13:54 -0400

    Server - added dependencies and setup entry app

--- server/index.js
+++ server/index.js
@@ -1,22 +1,43 @@
-require('dotenv')
-const express = require('express')
-const cors = require('cors')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoute = require("./routes/auth");
 
 const {
-    NODE_ENV, 
-    PORT
-} = process.env
+  NODE_ENV,
+  PORT,
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOST,
+  MONGO_DATABASE,
+} = process.env;
 
-app.use(express.json())
+// Setup mongoose db
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+const MONGO_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(MONGO_URL, MONGO_OPTIONS);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(cors());
 
-app.get("/", (req, res) => {
-    return res.json({
-        success:true,
-        message: "OK",
-        data: {}
-    })
-})
+app.use(authRoute);
 
-app.listen(PORT, "0.0.0.0", () => console.log(`Server started`))
+// error handler
+app.use((err, req, res, next) => {
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server started on port ${PORT}`)
+);
